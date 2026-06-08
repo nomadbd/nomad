b1f2dfe75c2e7f9fca69167db4d7d491f86ad206
nomadbd <nomadbysh@gmail.com>
2026-06-08 08:02:12 +0600

    Update PaymentSection.js

--- components/PaymentSection.js
+++ components/PaymentSection.js
@@ -1,18 +1,16 @@
-import styles from '@/styles/PaymentSection.module.css';
+import styles from '@/styles/ProductModal.module.css';
 
 export default function PaymentSection({ paymentMethod, setPaymentMethod }) {
   const paymentNumbers = {
-    'Bkash': '01521731371', 'Nagad': '01521731371', 'Rocket': '01521731371', 
-    'Upay': '01521731371', 'Cellfin': '01521731371'
+    'Bkash': '01521731371', 'Nagad': '01521731371', 'Rocket': '01521731371', 'Upay': '01521731371', 'Cellfin': '01521731371'
   };
 
   return (
     <div className={styles.container}>
       <select name="method" required className={styles.inputField} value={paymentMethod} onChange={(e) => setPaymentMethod(e.target.value)}>
-        <option value="" disabled>PAYMENT GATEWAY</option>
+        <option value="" disabled selected>PAYMENT GATEWAY</option>
         <option value="Bkash">Bkash</option><option value="Nagad">Nagad</option>
-        <option value="Rocket">Rocket</option><option value="Upay">Upay</option>
-        <option value="Cellfin">Cellfin</option>
+        <option value="Rocket">Rocket</option><option value="Upay">Upay</option><option value="Cellfin">Cellfin</option>
       </select>
 
       {paymentMethod && <p className={styles.info}>SEND MONEY TO: {paymentNumbers[paymentMethod]}</p>}
