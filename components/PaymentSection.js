c31ec6cc15de8ecada8285d6403597c15a3ebfc0
nomadbd <nomadbysh@gmail.com>
2026-06-08 07:38:32 +0600

    Update PaymentSection.js

--- components/PaymentSection.js
+++ components/PaymentSection.js
@@ -2,19 +2,27 @@ import styles from '@/styles/PaymentSection.module.css';
 
 export default function PaymentSection({ paymentMethod, setPaymentMethod }) {
   const paymentNumbers = {
-    'Bkash': '01521731371', 'Nagad': '01521731371', 
-    'Rocket': '01521731371', 'Upay': '01521731371', 'Cellfin': '01521731371'
+    'Bkash': '01521731371',
+    'Nagad': '01521731371',
+    'Rocket': '01521731371',
+    'Upay': '01521731371',
+    'Cellfin': '01521731371'
   };
 
   return (
     <div className={styles.container}>
       <select 
-        name="method" required className={styles.inputField} 
-        value={paymentMethod} onChange={(e) => setPaymentMethod(e.target.value)}
+        name="method" 
+        required 
+        className={styles.inputField} 
+        value={paymentMethod}
+        onChange={(e) => setPaymentMethod(e.target.value)}
       >
         <option value="" disabled>PAYMENT GATEWAY</option>
-        <option value="Bkash">Bkash</option><option value="Nagad">Nagad</option>
-        <option value="Rocket">Rocket</option><option value="Upay">Upay</option>
+        <option value="Bkash">Bkash</option>
+        <option value="Nagad">Nagad</option>
+        <option value="Rocket">Rocket</option>
+        <option value="Upay">Upay</option>
         <option value="Cellfin">Cellfin</option>
       </select>
 
@@ -23,14 +31,21 @@ export default function PaymentSection({ paymentMethod, setPaymentMethod }) {
       )}
 
       <input 
-        type="tel" name="sender_no" placeholder="SENDER NO (11 Digits)" 
-        required pattern="01[0-9]{9}" className={styles.inputField} 
+        type="tel" 
+        name="sender_no" 
+        placeholder="SENDER NO (11 Digits)" 
+        required 
+        pattern="01[0-9]{9}" 
+        className={styles.inputField} 
       />
       <input 
-        type="text" name="txn_id" placeholder="TRANSACTION ID" 
-        required minLength="6" className={styles.inputField} 
+        type="text" 
+        name="txn_id" 
+        placeholder="TRANSACTION ID" 
+        required 
+        minLength="6" 
+        className={styles.inputField} 
       />
     </div>
   );
 }
- 
