8f1bd0c7dc323424482a1b7e340f7c8626a21a2b
nomadbd <nomadbysh@gmail.com>
2026-06-09 13:35:35 +0600

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+// app/layout.tsx
+import './globals.css';
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-black text-white antialiased">{children}</body>
+    </html>
+  );
+}
