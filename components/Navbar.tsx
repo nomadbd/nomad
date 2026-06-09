2d892ab607cb571ca6b743641dba588637ff33db
nomadbd <nomadbysh@gmail.com>
2026-06-09 21:20:08 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,19 @@
 export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 bg-black border-b border-zinc-900 py-8">
-      <div className="flex flex-col items-center justify-center">
-        {/* লোগো */}
-        <h1 
-          className="text-3xl font-black text-white tracking-[0.4em] uppercase"
-          style={{ paddingLeft: '0.4em' }} // সেন্টার ঠিক করার জন্য সামান্য প্যাডিং
-        >
-          NOMAD
-        </h1>
+      <div className="flex flex-col items-center justify-center gap-0">
         
-        {/* স্লোগান: লোগোর সাথে মিল রেখে স্পেসিং */}
-        <p className="text-zinc-500 italic text-[10px] uppercase tracking-[0.4em] mt-1 ml-[0.4em]">
+        {/* N O M A D: ফ্লেক্স দিয়ে প্রতিটি অক্ষর আলাদা করে সাজানো */}
+        <div className="flex gap-[0.5em] text-3xl font-black text-white">
+          <span>N</span>
+          <span>O</span>
+          <span>M</span>
+          <span>A</span>
+          <span>D</span>
+        </div>
+        
+        {/* স্লোগান: লোগোর সাথে সামঞ্জস্যপূর্ণ স্পেসিং */}
+        <p className="text-zinc-500 italic text-[10px] uppercase tracking-[0.2em] mt-1 text-center">
           The one. Everywhere.
         </p>
       </div>
