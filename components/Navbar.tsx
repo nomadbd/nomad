c2c471ddd3183e0aeccf945b23a0ab1605f27909
nomadbd <nomadbysh@gmail.com>
2026-06-10 06:03:21 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,11 @@
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 bg-black border-b border-zinc-900 py-8 w-full">
-      <div className="flex flex-col items-center justify-center w-full">
-        {/* N O M A D লেখাটি সেন্টার করার জন্য text-center যোগ করা হয়েছে */}
-        <h1 className="text-4xl font-black text-center">
+    <nav className="sticky top-0 z-50 bg-black border-b border-zinc-900 py-8 w-full flex justify-center">
+      <div className="flex flex-col items-center justify-center text-center w-full">
+        <h1 className="text-4xl font-black block w-full text-center">
           N O M A D
         </h1>
-        
-        {/* স্লোগান লোগোর কাছে এবং সেন্টারে */}
-        <p className="text-zinc-500 italic text-sm mt-1 text-center">
+        <p className="text-zinc-500 italic text-sm mt-1 block w-full text-center">
           The one. Everywhere.
         </p>
       </div>
