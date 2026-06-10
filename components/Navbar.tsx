1f257f290394ef6ec8a088556211e953fed3221d
nomadbd <nomadbysh@gmail.com>
2026-06-10 06:10:38 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 bg-black border-b border-zinc-900 py-8 w-full flex justify-center">
-      <div className="flex flex-col items-center justify-center text-center w-full">
-        <h1 className="text-4xl font-black block w-full text-center">
+    <nav className="sticky top-0 z-50 bg-black border-b border-zinc-900 py-8 w-full">
+      {/* flex-col এবং items-center এখানেও নিশ্চিত করা হলো */}
+      <div className="flex flex-col items-center justify-center w-full">
+        {/* mx-auto যোগ করা হয়েছে যা লেফট/রাইট মার্জিন সমান রাখবে */}
+        <h1 className="text-4xl font-black text-center w-full mx-auto">
           N O M A D
         </h1>
-        <p className="text-zinc-500 italic text-sm mt-1 block w-full text-center">
+        <p className="text-zinc-500 italic text-sm mt-1 text-center w-full mx-auto">
           The one. Everywhere.
         </p>
       </div>
