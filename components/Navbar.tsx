6784223c04e5d311359136ce1d3644c749e5fe3b
nomadbd <nomadbysh@gmail.com>
2026-06-10 06:39:09 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,8 @@ export default function Navbar() {
         <h1 style={{ fontSize: '2.25rem', fontWeight: '900', margin: 0, padding: 0 }}>
           N O M A D
         </h1>
-        <p style={{ color: '#71717a', fontStyle: 'italic', fontSize: '0.875rem', marginTop: '4px' }}>
-          The one. Everywhere.
+        <p style={{ color: '#71717a',fontSize: '0.875rem', marginTop: '4px' }}>
+          𝚃𝚑𝚎 𝚘𝚗𝚎. 𝙴𝚟𝚎𝚛𝚢𝚠𝚑𝚎𝚛𝚎.
         </p>
       </div>
     </nav>
