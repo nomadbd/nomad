e9d68be65ee9b2885b6bf40ad40fd832a178a7e0
nomadbd <nomadbysh@gmail.com>
2026-06-13 16:45:35 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import Header from './components/Header';
+import React, { lazy, Suspense } from 'react';
+
+// এটি কোডটিকে ছোট ছোট টুকরোয় ভাগ করে লোড করবে, যা স্পিড বাড়ায়
+const Header = lazy(() => import('./components/Header'));
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-black">
-      <Header />
+      <Suspense fallback={<div></div>}>
+        <Header />
+      </Suspense>
       <main className="p-8">
       </main>
     </div>
