f14001bb541d728f0611609152f1f8ef5dc45c87
nomadbd <nomadbysh@gmail.com>
2026-06-13 10:10:35 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
- 
+import React from 'react';
+import Header from './components/Header';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <main className="p-8">
+        {/* আপনার মেইন কন্টেন্ট এখানে থাকবে */}
+      </main>
+    </div>
+  );
+};
+
+export default App; 
