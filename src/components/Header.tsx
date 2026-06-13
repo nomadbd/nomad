01c71a7f31d29eb9879343a434fe46700f75a3ca
nomadbd <nomadbysh@gmail.com>
2026-06-13 19:32:06 +0600

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-Import React from 'react';
+import React from 'react';
 
 const Header: React.FC = () => {
   return (
@@ -31,7 +31,7 @@ const Header: React.FC = () => {
         {/* WhatsApp SVG - Bolded */}
         <a href="https://wa.me/8801521731371" target="_blank" rel="noopener noreferrer" style={{ display: 'block' }}>
           <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
+            <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8.5 8.5v.5z" />
           </svg>
         </a>
       </div>
@@ -40,4 +40,3 @@ const Header: React.FC = () => {
 };
 
 export default Header;
-
