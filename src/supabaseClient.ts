6946632c3455c2403676c6874170462108a80c64
nomadbd <nomadbysh@gmail.com>
2026-06-16 12:28:07 +0600

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,7 @@
-// src/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-// Vercel-এর Environment Variables থেকে স্বয়ংক্রিয়ভাবে তথ্যগুলো চলে আসবে
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// Vite এর জন্য env রিড করার নিয়ম
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
