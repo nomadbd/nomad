055cb22f8f43ef4a6958e062776d382be25a2908
nomadbd <nomadbysh@gmail.com>
2026-06-18 20:10:35 +0600

    Delete Toast.tsx

--- src/components/Toast.tsx
+++
@@ -1,5 +0,0 @@
-export const Toast = ({ message, color }: { message: string, color: string }) => (
-  <div style={{ position: 'fixed', top: '20px', right: '20px', background: '#111', color: '#fff', padding: '15px 25px', borderRadius: '5px', borderLeft: `5px solid ${color}`, zIndex: 9999, fontSize: '12px', letterSpacing: '1px' }}>
-    {message}
-  </div>
-);
