e2935e7f234e79bb5db578dd5c37b5dc1c0b8aa1
nomadbd <nomadbysh@gmail.com>
2026-08-25 18:17:36 +0600

    Create index.ts

---
+++ src/components/admin/index.ts
@@ -0,0 +1,7 @@
+export { default as AdminCustomers } from './AdminCustomers';
+export { default as AdminLogistics } from './AdminLogistics';
+export { default as AdminOrders } from './AdminOrders';
+export { default as AdminOverview } from './AdminOverview';
+export { default as AdminProducts } from './AdminProducts';
+export { default as AdminStaff } from './AdminStaff';
+export { default as StaffProfile } from './StaffProfile';
