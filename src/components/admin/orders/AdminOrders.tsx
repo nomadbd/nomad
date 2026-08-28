999522b88582bb62c5997964cea2ac490fa2b005
nomadbd <nomadbysh@gmail.com>
2026-08-28 17:12:59 +0600

    Update AdminOrders.tsx

--- src/components/admin/orders/AdminOrders.tsx
+++ src/components/admin/orders/AdminOrders.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useMemo } from 'react';
 import '../admin-animations.css';
 
 import { Order } from '../../../utils/messageUtils';
-import { handlePrintInvoice, handlePrintBulkInvoices } from '../../utils/invoiceUtils';
+import { handlePrintInvoice, handlePrintBulkInvoices } from '../../../utils/invoiceUtils';
 import OrderCard from './OrderCard';
 import BulkMessageView from './BulkMessageView';
 import OrderStatusModal from './OrderStatusModal';
