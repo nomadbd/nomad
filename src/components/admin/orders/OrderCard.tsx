420166b515b6bf9c90973a3508dd57806c320021
nomadbd <nomadbysh@gmail.com>
2026-08-28 19:30:18 +0600

    Update OrderCard.tsx

--- src/components/admin/orders/OrderCard.tsx
+++ src/components/admin/orders/OrderCard.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { CheckIcon, CloseIcon, EditIcon, ShareIcon } from '../../icons';
-import { Order, TEMPLATE_PRESETS, formatWhatsAppNumber, renderPersonalizedText } from '../../../utils/messageUtils';
+import { CheckIcon, CloseIcon, EditIcon, ShareIcon } from '@/components/icons';
+import { Order, TEMPLATE_PRESETS, formatWhatsAppNumber, renderPersonalizedText } from '@/utils/messageUtils';
 
 
 const BD_COURIERS = [
