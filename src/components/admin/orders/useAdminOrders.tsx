cd4233e5fb6e3ff1f9e0bca33a12bb8f5b5a8733
nomadbd <nomadbysh@gmail.com>
2026-08-28 17:44:32 +0600

    Update useAdminOrders.tsx

--- src/components/admin/orders/useAdminOrders.tsx
+++ src/components/admin/orders/useAdminOrders.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../../supabaseClient';
+import { supabase } from '../../../supabaseClient';
 import { Order } from '../../../utils/messageUtils';
 import { SupabaseOrderResponse } from './adminOrders.types';
 
