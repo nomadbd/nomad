28f4edf8b5b71c002440656a0fb3213933c4c975
nomadbd <nomadbysh@gmail.com>
2026-08-29 08:25:14 +0600

    Update ProductShowroomCard.tsx

--- src/components/admin/products/ProductShowroomCard.tsx
+++ src/components/admin/products/ProductShowroomCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Product } from './products.types';
+import { Product } from './products.type';
 import { ProductMediaViewer } from './ProductMediaViewer';
 import { ProductActionToolbar } from './ProductActionToolbar';
 
