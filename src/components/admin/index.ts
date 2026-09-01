1135096af53c4993342f448f80340a7917e2d2df
nomadbd <nomadbysh@gmail.com>
2026-09-01 19:07:28 +0600

    Update index.ts

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -1,5 +1,5 @@
 export { default as AdminCustomers } from './AdminCustomers';
-export { default as AdminLogistics } from './AdminLogistics';
+export { default as AdminLogistics } from './logistics/AdminLogistics';
 export { default as AdminOrders } from './orders/AdminOrders';
 export { default as AdminOverview } from './overview/AdminOverview';
 export { default as AdminProducts } from './products/AdminProducts';
