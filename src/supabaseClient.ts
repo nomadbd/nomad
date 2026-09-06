1e20eb00b8040346e05ae5a4be052d36e1da3697
nomadbd <nomadbysh@gmail.com>
2026-09-06 20:33:13 +0600

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Vite এর জন্য env রিড করার নিয়ম
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
