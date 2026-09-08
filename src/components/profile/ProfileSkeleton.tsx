113c7340c56d1dc40936255c4225889896834291
nomadbd <nomadbysh@gmail.com>
2026-09-08 06:49:59 +0600

    Create ProfileSkeleton.tsx

---
+++ src/components/profile/ProfileSkeleton.tsx
@@ -0,0 +1,9 @@
+export default function ProfileSkeleton() {
+  return (
+    <div style={{ opacity: 0.3, width: '100%', marginTop: '20px' }}>
+      <div style={{ height: '24px', width: '40%', background: '#333', marginBottom: '25px', borderRadius: '4px' }}></div>
+      <div style={{ height: '13px', width: '20%', background: '#333', marginBottom: '10px', borderRadius: '4px' }}></div>
+      <div style={{ height: '18px', width: '60%', background: '#333', borderRadius: '4px' }}></div>
+    </div>
+  );
+}
