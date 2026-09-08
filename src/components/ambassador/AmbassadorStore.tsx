75f55f97f0c83f75389bdc75463ca6815057b2f6
nomadbd <nomadbysh@gmail.com>
2026-09-08 12:20:21 +0600

    Update AmbassadorStore.tsx

--- src/components/ambassador/AmbassadorStore.tsx
+++ src/components/ambassador/AmbassadorStore.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { supabase } from '../../supabaseClient';
+import { supabase } from '@/supabaseClient';
 
 export default function AmbassadorStore() {
   const { slug } = useParams<{ slug: string }>();
