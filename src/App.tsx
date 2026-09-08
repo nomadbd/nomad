2643ffd99d227482d59eab54e3923f35e8c5e19e
nomadbd <nomadbysh@gmail.com>
2026-09-08 08:12:01 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useSession } from './hooks/useSession';
 
 import Profile from './pages/Profile';
 
+import AmbassadorDashboard from './pages/AmbassadorDashboard';
 
 import Header from './components/Header';
 import SearchOverlay from './components/SearchOverlay';
