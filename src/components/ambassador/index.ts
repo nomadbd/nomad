84359a99f923a65db905974efa11594bdcefa052
nomadbd <nomadbysh@gmail.com>
2026-09-08 12:32:05 +0600

    Create index.ts

---
+++ src/components/ambassador/index.ts
@@ -0,0 +1,5 @@
+import { lazy } from 'react';
+
+export const Ambassador = lazy(() => import('./Ambassador'));
+export const AmbassadorJoin = lazy(() => import('./AmbassadorJoin'));
+export const AmbassadorStore = lazy(() => import('./AmbassadorStore'));
