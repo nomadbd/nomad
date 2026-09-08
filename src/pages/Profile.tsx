b1677bbd28c86e71bc70b3c3f0a9a75281f356e5
nomadbd <nomadbysh@gmail.com>
2026-09-08 20:07:31 +0600

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -7,7 +7,7 @@ import Toast from '../components/ui/Toast';
 import ProfileSkeleton from '../components/profile/ProfileSkeleton';
 import ProfileHeader from '../components/profile/ProfileHeader';
 import AmbassadorDashboard from './AmbassadorDashboard';
-import SettingsView from '../components/profile/ProfileSettings';
+import ProfileSettings from '../components/profile/ProfileSettings';
 import ImageCropModal from '../components/ui/ImageCropModal';
 
 export default function Profile() {
@@ -426,7 +426,7 @@ export default function Profile() {
             )}
           </>
         ) : (
-          <SettingsView 
+          <ProfileSettings 
             profile={profile} 
             avatarUrl={avatarUrl} 
             isAmbassadorActive={isAmbassadorActive} 
