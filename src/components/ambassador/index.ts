cea8abb2d2fc81088b716ed32e3176e28346c3e4
nomadbd <nomadbysh@gmail.com>
2026-09-08 12:42:24 +0600

    Delete index.ts

--- src/components/ambassador/index.ts
+++
@@ -1,5 +0,0 @@
-import { lazy } from 'react';
-
-export const Ambassador = lazy(() => import('./Ambassador'));
-export const AmbassadorJoin = lazy(() => import('./AmbassadorJoin'));
-export const AmbassadorStore = lazy(() => import('./AmbassadorStore'));
