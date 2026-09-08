bc155a5a5a021355713a8ffabe1cd9792e0ed3be
nomadbd <nomadbysh@gmail.com>
2026-09-08 08:10:29 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { CartProvider } from './context/CartContext';
 import { useSession } from './hooks/useSession';
 
 import Profile from './pages/Profile';
-import AmbassadorDashboard from './pages/AmbassadorDashboard';
+
 
 import Header from './components/Header';
 import SearchOverlay from './components/SearchOverlay';
