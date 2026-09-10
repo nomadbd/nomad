23e03935efc286be5572f68eda3b36e57ef35ae3
nomadbd <nomadbysh@gmail.com>
2026-09-10 15:38:03 +0600

    Create SearchIcon.tsx

---
+++ src/components/icons/SearchIcon.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+  color?: string;
+}
+
+const SearchIcon: React.FC<IconProps> = ({ size = 24, color = 'white', ...props }) => (
+  <svg 
+    width={size} 
+    height={size} 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke={color} 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round" 
+    {...props}
+  >
+    <path d="m21 21-4.34-4.34"/>
+    <circle cx="11" cy="11" r="8"/>
+  </svg>
+);
+
+export default SearchIcon;
