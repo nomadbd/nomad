d0f19f0e952eabb732c1fb5b432afd933a5859f5
nomadbd <nomadbysh@gmail.com>
2026-09-10 16:21:12 +0600

    Update CartIcon.tsx

--- src/components/icons/CartIcon.tsx
+++ src/components/icons/CartIcon.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
-interface IconProps extends React.SVGProps<SVGSVGElement> {
-  size?: number;
-  color?: string;
-}
-
-export const CartIcon: React.FC<IconProps> = ({ size = 24, color = 'white', ...props }) => (
-  <svg 
-    width={size} 
-    height={size} 
-    viewBox="0 0 24 24" 
-    fill="none" 
-    stroke={color} 
-    strokeWidth="2" 
-    strokeLinecap="round" 
-    strokeLinejoin="round" 
-    {...props}
-  >
-    <path d="M2.048 18.566A2 2 0 0 0 4 21h16a2 2 0 0 0 1.952-2.434l-2-9A2 2 0 0 0 18 8H6a2 2 0 0 0-1.952 1.566z"/>
-    <path d="M8 11V6a4 4 0 0 1 8 0v5"/>
-  </svg>
-);
+export const CartIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
+  width = 24,
+  height = 24,
+  stroke = "currentColor",
+  fill = "none",
+  strokeWidth = 2,
+  ...props
+}) => {
+  return (
+    <svg 
+      width={width} 
+      height={height} 
+      viewBox="0 0 24 24" 
+      fill={fill} 
+      stroke={stroke} 
+      strokeWidth={strokeWidth} 
+      strokeLinecap="round" 
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M2.048 18.566A2 2 0 0 0 4 21h16a2 2 0 0 0 1.952-2.434l-2-9A2 2 0 0 0 18 8H6a2 2 0 0 0-1.952 1.566z"/>
+      <path d="M8 11V6a4 4 0 0 1 8 0v5"/>
+    </svg>
+  );
+};
