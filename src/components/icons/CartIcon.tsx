8771892935052215bd6925ece7a5efb19e39dd34
nomadbd <nomadbysh@gmail.com>
2026-09-10 15:38:44 +0600

    Create CartIcon.tsx

---
+++ src/components/icons/CartIcon.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+  color?: string;
+}
+
+const CartIcon: React.FC<IconProps> = ({ size = 24, color = 'white', ...props }) => (
+  <svg 
+    width={size} 
+    height={size} 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke={color} 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round" 
+    {...props}
+  >
+    <path d="M2.048 18.566A2 2 0 0 0 4 21h16a2 2 0 0 0 1.952-2.434l-2-9A2 2 0 0 0 18 8H6a2 2 0 0 0-1.952 1.566z"/>
+    <path d="M8 11V6a4 4 0 0 1 8 0v5"/>
+  </svg>
+);
+
+export default CartIcon;
