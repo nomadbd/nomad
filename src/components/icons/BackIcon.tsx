dd0aae07a2e01b97078e35da9978711ef1ba5731
nomadbd <nomadbysh@gmail.com>
2026-09-12 20:20:15 +0600

    Create BackIcon.tsx

---
+++ src/components/icons/BackIcon.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export interface IconProps extends React.SVGProps<SVGSVGElement> {
+  size?: number | string;
+  color?: string;
+  strokeWidth?: number | string;
+}
+
+export const BackIcon: React.FC<IconProps> = ({
+  size = 20,
+  color = 'currentColor',
+  strokeWidth = 2.2,
+  ...props
+}) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M15 18l-6-6 6-6" />
+    </svg>
+  );
+};
+
+export default BackIcon;
