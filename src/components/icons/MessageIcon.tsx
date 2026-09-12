82b77f5566208f8caf08a24f62452a543f873822
nomadbd <nomadbysh@gmail.com>
2026-09-12 20:36:06 +0600

    Create MessageIcon.tsx

---
+++ src/components/icons/MessageIcon.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const MessageIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
+  width = 18,
+  height = 18,
+  stroke = 'currentColor',
+  fill = 'none',
+  strokeWidth = 2,
+  ...props
+}) => (
+  <svg
+    width={width}
+    height={height}
+    viewBox="0 0 24 24"
+    fill={fill}
+    stroke={stroke}
+    strokeWidth={strokeWidth}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
+  </svg>
+);
+
+export default MessageIcon;
