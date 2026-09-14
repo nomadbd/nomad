718191fb05106eba24462cf91fcbb311cdb0caf7
nomadbd <nomadbysh@gmail.com>
2026-09-14 12:30:52 +0600

    Update ProfileSettings.tsx

--- src/components/profile/ProfileSettings.tsx
+++ src/components/profile/ProfileSettings.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, RefObject } from 'react';
-import { isUserSubscribed, subscribeUserToPush, unsubscribeUserFromPush } from '../utils/pushManager';
+import { isUserSubscribed, subscribeUserToPush, unsubscribeUserFromPush } from '@/utils/pushManager';
 
 interface ProfileSettingsProps {
   profile: any;
