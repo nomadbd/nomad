1269a49aac2ce5c451c579d66447224c5ba17e24
nomadbd <nomadbysh@gmail.com>
2026-09-15 19:15:58 +0600

    Update AdminNotifications.tsx

--- src/components/admin/notifications/AdminNotifications.tsx
+++ src/components/admin/notifications/AdminNotifications.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../../supabaseClient';
-import { HistoryIcon } from '../../icons';
+import { HistoryIcon } from '../../../icons';
 import NotificationLogs from './NotificationLogs';
 
 interface UserProfile {
