19379d0ccff298b2fdeb26bb61bce6277a03605f
nomadbd <nomadbysh@gmail.com>
2026-09-16 11:35:56 +0600

    Create ScheduleIcon.tsx

---
+++ src/components/icons/ScheduleIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const ScheduleIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <circle cx="12" cy="12" r="10" />
+    <polyline points="12 6 12 12 16 14" />
+  </svg>
+);
+
+export default ScheduleIcon;
