9409bd13f532e7043acddac69f765c2590595967
nomadbd <nomadbysh@gmail.com>
2026-09-17 22:20:50 +0600

    Create index.ts

---
+++ src/components/ambassador/join/index.ts
@@ -0,0 +1,7 @@
+export * from './types';
+export * from './HeroHeader';
+export * from './BenefitCards';
+export * from './ExpiredNotice';
+export * from './JoinSheet';
+export * from './ConciergeModal';
+export * from './JoinFooter';
