2b65245218bafa116ed52b1df8a730b7ad022b6a
nomadbd <nomadbysh@gmail.com>
2026-09-19 23:23:46 +0600

    Update index.ts

--- src/components/admin/messages/index.ts
+++ src/components/admin/messages/index.ts
@@ -1,3 +1,9 @@
 export { AdminMessages, default } from './AdminMessages';
 export * from './types';
 export * from './useAdminMessages';
+export * from './MessageSidebar';
+export * from './ChatHeader';
+export * from './MessageList';
+export * from './ChatInput';
+export * from './NewChatModal';
+export * from './UserDetailDrawer';
